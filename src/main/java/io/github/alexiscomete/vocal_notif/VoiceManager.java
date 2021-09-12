a0094de3e676d06a8a22edef8ebcd766d28abcc7
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-09-12 19:11:11 +0200

    bug-- bug++

--- src/main/java/io/github/alexiscomete/vocal_notif/VoiceManager.java
+++ src/main/java/io/github/alexiscomete/vocal_notif/VoiceManager.java
@@ -8,9 +8,7 @@ import org.javacord.api.entity.server.invite.InviteBuilder;
 import org.javacord.api.entity.user.User;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class VoiceManager {
 
@@ -25,18 +23,18 @@ public class VoiceManager {
     public static void switchUser(long serverID, long userID) {
         CompletableFuture<User> op = Main.api.getUserById(userID);
         op.thenAcceptAsync(user -> {
-            SaveLocation<User> server = getServer(serverID);
+            SaveLocation<Long> server = getServer(serverID);
             if (server == null) {
                 System.out.println("null");
                 return;
             }
             System.out.println(user.getId());
-            if (contains(server.getContent(), user)) { // the bug
+            if (server.getContent().contains(user.getId())) { // the bug
                 System.out.println("co");
-                server.getContent().remove(user);
+                server.getContent().remove(user.getId());
             } else {
                 System.out.println("n");
-                server.getContent().add(user);
+                server.getContent().add(user.getId());
             }
             server.saveAll();
         });
@@ -47,27 +45,27 @@ public class VoiceManager {
         System.out.println("Invite ->");
         inviteF.thenAcceptAsync(invite -> {
             System.out.println("<-");
-            SaveLocation<User> server = getServer(serv.getId());
+            SaveLocation<Long> server = getServer(serv.getId());
             if (server == null) {
                 return;
             }
             System.out.println("notif");
-            for (User user : server.getContent()) {
-                System.out.println("User");
-                if (user != null && voiceChannel.canConnect(user) && user.getId() != us.getId()) {
-                    user.sendMessage("Salut ! <@" + us.getId() + "> (" + us.getName() + ")  est en vocal sur un serveur ... clique sur le lien pour le rejoindre : https://discord.gg/" + invite.getCode());
-                }
+            for (Long userId : server.getContent()) {
+                CompletableFuture<User> coUser = Main.api.getUserById(userId);
+                coUser.thenAcceptAsync(user -> {
+                    System.out.println("User");
+                    if (user != null && voiceChannel.canConnect(user) && userId != us.getId()) {
+                        user.sendMessage("Salut ! <@" + us.getId() + "> (" + us.getName() + ")  est en vocal sur un serveur ... clique sur le lien pour le rejoindre : https://discord.gg/" + invite.getCode());
+                    }
+                });
             }
         });
     }
 
 
-    public static SaveLocation<User> getServer(long server) {
+    public static SaveLocation<Long> getServer(long server) {
         try {
-            SaveLocation<User> saveLocation = new SaveLocation<>(" ", "/voice-save/" + server + ".txt", VoiceManager::toUser, ar -> {
-                long id = ar.getId();
-                return String.valueOf(id);
-            });
+            SaveLocation<Long> saveLocation = new SaveLocation<>(" ", "/voice-save/" + server + ".txt", Long::parseLong, String::valueOf);
             saveLocation.loadAll();
             return saveLocation;
         } catch (IOException e) {
@@ -76,41 +74,4 @@ public class VoiceManager {
         }
     }
 
-    public static User toUser(String str) {
-        System.out.println("to user : " + str);
-        CompletableFuture<User> op = Main.api.getUserById(Long.parseLong(str)); //src of the bug ?
-        AtomicReference<User> user = new AtomicReference<>();
-        VoiceManager waitUser = new VoiceManager();
-        op.thenAcceptAsync(us -> {
-            user.set(us);
-            System.out.println("accept");
-            waitUser.notifyAll();
-        });
-        waitUser.waitForUser();
-        System.out.println(user.get());
-        return user.get();
-    }
-
-    public static boolean contains(ArrayList<User> users, User user) {
-        System.out.println(users.size());
-        for (User u : users) {
-            if (u != null) {
-                System.out.println(u.getId());
-                System.out.println(user.getId());
-                System.out.println(u.getId() == user.getId());
-                if (u.getId() == user.getId()) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public void waitForUser() {
-        try {
-            this.wait();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 }
